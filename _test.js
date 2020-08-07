519c092b22f5f491a3346756ce63dc162a226680
Mitrich <flaming.dmitry@gmail.com>
2020-08-07 19:59:02 +0300

    add login test

--- _test.js
+++ _test.js
@@ -22,3 +22,20 @@ Scenario("registering wallet", async (I) => {
   I.click("DONE");
   I.click("ENABLE");
 });
+
+Scenario("login", (I) => {
+  I.click("#com.wallet.crypto.trustapp:id/import_account_action");
+  I.checkOption("#com.wallet.crypto.trustapp:id/acceptCheckBox");
+  I.click("CONTINUE");
+  I.click("Multi-Coin Wallet");
+  const pass =
+    "bean middle champion also donor shaft tongue pet trumpet stereo render crew";
+  I.fillField(
+    "//hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/androidx.viewpager.widget.ViewPager/android.widget.ScrollView/android.view.ViewGroup/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.TextView",
+    pass
+  );
+  I.hideDeviceKeyboard();
+  I.click("IMPORT");
+  I.wait(30);
+  pause();
+});
